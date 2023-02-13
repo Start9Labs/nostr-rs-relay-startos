50651dc572983f8f7016a728c872ff0a6433249a
Chris Guida <chrisguida@gmail.com>
2023-02-13 15:29:57 -0600

    convert to use config builder

--- scripts/deps.ts
+++ scripts/deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.8/mod.ts";
+export * from "https://raw.githubusercontent.com/Start9Labs/embassy-sdk-ts/9430d1ceb21ecc21e6abef0038411a8a373c5661/mod.ts";
 export * as TOML from "https://deno.land/std@0.177.0/encoding/toml.ts";
