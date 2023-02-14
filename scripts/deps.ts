384bb96093efa9fd56e976a9c16125ccd1ed4c9a
BluJ <mogulslayer@gmail.com>
2023-02-14 10:25:56 -0700

    chore: Update to use the latest of the builder pattern

--- scripts/deps.ts
+++ scripts/deps.ts
@@ -1,2 +1,2 @@
-export * from "https://raw.githubusercontent.com/Start9Labs/embassy-sdk-ts/9430d1ceb21ecc21e6abef0038411a8a373c5661/mod.ts";
+export * from "https://raw.githubusercontent.com/Start9Labs/embassy-sdk-ts/757033d5030812980ce388bb91015b37c98c2156/mod.ts";
 export * as TOML from "https://deno.land/std@0.177.0/encoding/toml.ts";
--- scripts/procedures/getConfig.ts
+++ scripts/procedures/getConfig.ts
@@ -3,30 +3,31 @@ import { C, compat } from "../deps.ts";
 export const [getConfig, setConfigMatcher] = compat.getConfigAndMatcher(
   C.Config.withValue(
     "tor-address",
-    C.Value.pointer(C.Pointer.packageTorAddress(
-      {
+    C.Value.pointer(
+      C.Pointer.packageTorAddress({
         name: "Tor Address",
         description: "The Tor address for the websocket server.",
         warning: null,
         "package-id": "nostr",
         interface: "websocket",
-      },
-    )),
-  ).addValue(
-    "lan-address",
-    C.Value.pointer(C.Pointer.packageLanAddress(
-      {
-        name: "LAN Address",
-        description: "The LAN address for the websocket server.",
-        warning: null,
-        "package-id": "nostr",
-        interface: "websocket",
-      },
-    )),
-  ).addValue(
-    "relay-type",
-    C.Value.union(
-      {
+      })
+    )
+  )
+    .withValue(
+      "lan-address",
+      C.Value.pointer(
+        C.Pointer.packageLanAddress({
+          name: "LAN Address",
+          description: "The LAN address for the websocket server.",
+          warning: null,
+          "package-id": "nostr",
+          interface: "websocket",
+        })
+      )
+    )
+    .withValue(
+      "relay-type",
+      C.Value.union({
         "display-as": null,
         "unique-by": null,
         default: "private",
@@ -34,37 +35,40 @@ export const [getConfig, setConfigMatcher] = compat.getConfigAndMatcher(
           "Private or public. A private relay (highly recommended) restricts write access to specific pubkeys. Anyone can write to a public relay.",
         name: "Relay Type",
         warning: null,
-        "tag": {
-          "id": "type",
-          "name": "Relay Type",
-          "description":
+        tag: {
+          id: "type",
+          name: "Relay Type",
+          description:
             "Private or public. A private relay (highly recommended) restricts write access to specific pubkeys. Anyone can write to a public relay.",
+          warning: null,
           "variant-names": {
-            "private": "Private",
-            "public": "Public",
+            private: "Private",
+            public: "Public",
           },
         },
         variants: C.Variants.withVariant(
           "private",
           C.Config.withValue(
             "pubkey_whitelist",
-            C.Value.list(C.List.string({
-              name: "Pubkey Whitelist (Hex)",
-              description:
-                "A list of pubkeys that are permitted to publish through your relay. A minimum, you need to enter your own Nostr hex (not npub) pubkey. Go to https://damus.io/key/ to convert from npub to hex.",
-              warning: null,
-              default: [],
-              range: "[1,*)",
-              spec: {
-                copyable: null,
-                masked: null,
-                placeholder: "hex (not npub) pubkey",
-                pattern: "[0-9a-fA-F]{64}",
-                "pattern-description":
-                  "Must be a valid 64-digit hexadecimal value (ie a Nostr hex pubkey, not an npub). Go to https://damus.io/key/ to convert npub to hex.",
-              },
-            })),
-          ),
+            C.Value.list(
+              C.List.string({
+                name: "Pubkey Whitelist (Hex)",
+                description:
+                  "A list of pubkeys that are permitted to publish through your relay. A minimum, you need to enter your own Nostr hex (not npub) pubkey. Go to https://damus.io/key/ to convert from npub to hex.",
+                warning: null,
+                default: [],
+                range: "[1,*)",
+                spec: {
+                  copyable: null,
+                  masked: null,
+                  placeholder: "hex (not npub) pubkey",
+                  pattern: "[0-9a-fA-F]{64}",
+                  "pattern-description":
+                    "Must be a valid 64-digit hexadecimal value (ie a Nostr hex pubkey, not an npub). Go to https://damus.io/key/ to convert npub to hex.",
+                },
+              })
+            )
+          )
         ).withVariant(
           "public",
           C.Config.withValue(
@@ -77,12 +81,11 @@ export const [getConfig, setConfigMatcher] = compat.getConfigAndMatcher(
               copyable: null,
               masked: null,
               placeholder: null,
-            }),
-          ),
+            })
+          )
         ),
-      },
-    ),
-  ).build(),
+      })
+    )
   // .addValue(
 
   // ).build(),
