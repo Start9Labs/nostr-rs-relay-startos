f5aacaf679dbc02b0a4798f301add51f8e284f93
Matt Hill <matthewonthemoon@gmail.com>
2023-05-07 08:08:28 -0600

    add migration

--- startos/procedures/migrations/index.ts
+++ startos/procedures/migrations/index.ts
@@ -1,7 +1,8 @@
 import { setupMigrations } from '@start9labs/start-sdk/lib/inits/migrations/setupMigrations'
 import { manifest } from '../../manifest'
+import { v0_8_9 } from './v0_8_9'
 
 /**
  * Add each new migration as the next argument to this function
  */
-export const migrations = setupMigrations(manifest)
+export const migrations = setupMigrations(manifest, v0_8_9)
