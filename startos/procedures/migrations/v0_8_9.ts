b87220931d10a758a91cfa9ab4c911962623acf5
Matt Hill <matthewonthemoon@gmail.com>
2023-05-08 11:36:15 -0600

    rmdir instead

--- startos/procedures/migrations/v0_8_9.ts
+++ startos/procedures/migrations/v0_8_9.ts
@@ -1,17 +1,11 @@
 import { Migration } from '@start9labs/start-sdk/lib/inits/migrations/Migration'
+import { rmdir } from 'fs/promises'
 
-/**
- * This is an example migration file
- *
- * By convention, each version service requiring a migration receives its own file
- *
- * The resulting migration (e.g. v4000) is exported, then imported into migration/index.ts
- */
 export const v0_8_9 = new Migration({
   version: '0.8.9',
   up: async ({ effects }) => {
     // remove old start9 dir
-    await effects.runCommand(['rm', '-rf', '/root/start9'])
+    await rmdir('/root/start9')
   },
   down: async ({ effects }) => {
     throw new Error('Downgrade not permitted')
