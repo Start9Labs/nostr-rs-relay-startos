53791e8a3c1e1bf9e6140af5fb42bf1118a343cc
Mariusz Kogen <k0gen@pm.me>
2025-09-03 21:13:31 +0200

    Makefile refactor

--- startos/manifest.ts
+++ startos/manifest.ts
@@ -1,14 +1,22 @@
 import { setupManifest } from '@start9labs/start-sdk'
+import { SDKImageInputSpec } from '@start9labs/start-sdk/base/lib/types/ManifestTypes'
+
+const BUILD = process.env.BUILD || ''
+
+const architectures =
+  BUILD === 'x86_64' || BUILD === 'aarch64' ? [BUILD] : ['x86_64', 'aarch64']
 
 export const manifest = setupManifest({
   id: 'nostr',
   title: 'Nostr RS Relay',
-  license: 'mit',
+  license: 'MIT',
   wrapperRepo: 'https://github.com/Start9Labs/nostr-rs-relay-startos/',
   upstreamRepo: 'https://sr.ht/~gheartsfield/nostr-rs-relay/',
   supportSite: 'https://todo.sr.ht/~gheartsfield/nostr-rs-relay/',
   marketingSite: 'https://nostr.com/',
   donationUrl: null,
+    docsUrl:
+    'https://github.com/Start9Labs/nostr-rs-relay/blob/master/instructions.md',
   description: {
     short: 'A Nostr relay, written in Rust',
     long: 'This is a Nostr relay, written in Rust. It currently supports the entire relay protocol, including pay-to-relay capabilities.',
@@ -19,9 +27,12 @@ export const manifest = setupManifest({
       source: {
         dockerTag: 'scsibug/nostr-rs-relay:0.9.0',
       },
-    },
+      arch: architectures,
+    } as SDKImageInputSpec,
   },
-  hardwareRequirements: {},
+  hardwareRequirements: {
+    arch: architectures,
+  },
   alerts: {
     install: null,
     update: null,
@@ -35,13 +46,13 @@ export const manifest = setupManifest({
       description:
         'Needed if you want to accept relay subscription payments using Core Lightning',
       optional: true,
-      s9pk: '../c-lightning-startos/c-lightning.s9pk',
+      s9pk: null,
     },
     lnbits: {
       description:
         'Needed if you want to accept relay subscription payments using LNBits',
       optional: true,
-      s9pk: '../lnbits-startos/lnbits.s9pk',
+      s9pk: null,
     },
   },
 })
