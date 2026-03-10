5c4a1e0c1c2b76c4aa7a5b4523cce3b45ea2b3fd
Matt Hill <mattnine@protonmail.com>
2026-03-10 08:27:37 -0600

    better docs

--- startos/manifest/index.ts
+++ startos/manifest/index.ts
@@ -1,5 +1,5 @@
 import { setupManifest } from '@start9labs/start-sdk'
-import { short, long } from './i18n'
+import { long, short } from './i18n'
 
 export const manifest = setupManifest({
   id: 'nostr-rs-relay',
@@ -10,7 +10,10 @@ export const manifest = setupManifest({
     'https://github.com/Start9Labs/nostr-rs-relay-startos/tree/update/040',
   upstreamRepo: 'https://sr.ht/~gheartsfield/nostr-rs-relay/',
   marketingUrl: 'https://nostr.com/',
-  docsUrls: ['https://github.com/scsibug/nostr-rs-relay'],
+  docsUrls: [
+    'https://github.com/scsibug/nostr-rs-relay/blob/master/README.md',
+    'https://github.com/scsibug/nostr-rs-relay/blob/master/config.toml',
+  ],
   description: { short, long },
   volumes: ['db', 'config', 'main'], // main for migration only
   images: {
