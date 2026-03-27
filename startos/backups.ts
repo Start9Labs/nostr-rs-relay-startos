281011fd98373ff69e8a618c0f07d2f0277d8085
Matt Hill <mattnine@protonmail.com>
2026-03-27 01:14:12 -0600

    don't back up main

--- startos/backups.ts
+++ startos/backups.ts
@@ -1,5 +1,5 @@
 import { sdk } from './sdk'
 
 export const { createBackup, restoreInit } = sdk.setupBackups(
-  async ({ effects }) => sdk.Backups.ofVolumes('main', 'db', 'config'),
+  async ({ effects }) => sdk.Backups.ofVolumes('db', 'config'),
 )
