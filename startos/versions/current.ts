dc9b8bf1504fa7360770afb07c28ab8d667c14d2
Matt Hill <9935159+MattDHill@users.noreply.github.com>
2026-07-17 14:00:40 -0600

    freeze migrations into v0.10.0_1.ts, bump to 0.10.0:2

--- startos/versions/current.ts
+++ startos/versions/current.ts
@@ -1,79 +1,13 @@
-import { IMPOSSIBLE, VersionInfo, YAML } from '@start9labs/start-sdk'
-import { execFile } from 'child_process'
-import { readdir, readFile, rm } from 'fs/promises'
-import { join } from 'path'
-import { configToml } from '../fileModels/config.toml'
+import { VersionInfo } from '@start9labs/start-sdk'
 
 export const current = VersionInfo.of({
-  version: '0.10.0:1',
+  version: '0.10.0:2',
   releaseNotes: {
-    en_US: 'Internal updates (start-sdk 2.0.x)',
-    es_ES: 'Actualizaciones internas (start-sdk 2.0.x)',
-    de_DE: 'Interne Aktualisierungen (start-sdk 2.0.x)',
-    pl_PL: 'Aktualizacje wewnętrzne (start-sdk 2.0.x)',
-    fr_FR: 'Mises à jour internes (start-sdk 2.0.x)',
+    en_US: 'Internal updates',
+    es_ES: 'Actualizaciones internas',
+    de_DE: 'Interne Aktualisierungen',
+    pl_PL: 'Aktualizacje wewnętrzne',
+    fr_FR: 'Mises à jour internes',
   },
-  migrations: {
-    up: async ({ effects }) => {
-      const mainVol = '/media/startos/volumes/main'
-
-      // get old config.yaml
-      const configYaml:
-        | {
-            'relay-type': {
-              type: 'private' | 'public'
-              pubkey_whitelist: string[]
-              info: {}
-              limits: {
-                event_kind_blacklist: string[]
-              }
-            }
-          }
-        | undefined = await readFile(
-        '/media/startos/volumes/main/start9/config.yaml',
-        'utf-8',
-      ).then(YAML.parse, () => undefined)
-
-      if (configYaml) {
-        const relayType = configYaml['relay-type']
-
-        // srv
-        await new Promise((res, rej) => {
-          execFile(
-            'sh',
-            ['-c', `mv ${mainVol}/data/* /media/startos/volumes/db`],
-            (err) => (err ? rej(err) : res(null)),
-          )
-        }).catch(console.error)
-
-        const old = relayType
-          ? relayType.type === 'private'
-            ? {
-                authorization: {
-                  pubkey_whitelist: relayType.pubkey_whitelist,
-                },
-              }
-            : {
-                info: relayType.info,
-                limits: {
-                  ...relayType.limits,
-                  event_kind_blacklist:
-                    relayType.limits?.event_kind_blacklist?.map(Number) ?? [],
-                },
-              }
-          : {}
-
-        await configToml.merge(effects, old)
-
-        // clear old main vol
-        const entries = await readdir(mainVol)
-        await Promise.all(
-          entries.map((entry) =>
-            rm(join(mainVol, entry), { recursive: true }).catch(console.error),
-          ),
-        )
-      }
-    },
-    down: IMPOSSIBLE,
-  },
+  migrations: {},
 })
